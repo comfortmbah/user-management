f4295672b5b318fbbe66350fc6d377d0534a4f0d
sylvester <sylvestermbah90@gmail.com>
2026-08-30 16:57:41 -0700

    Add users data and getUsers model function

---
+++ storages/userStorage.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+  },
+  {
+    id: 2,
+    firstName: "Jane",
+    lastName: "Smith",
+  },
+];
+
+export const getUsers = () => {
+  return users;
+}

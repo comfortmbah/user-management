96a240b2216883e69f6927e2dcfcaa3983bf046d
sylvester <sylvestermbah90@gmail.com>
2026-08-30 17:03:15 -0700

    Add route to user list controller

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { userListGet } from "../controllers/userController";
 
 const router = express.Router();
+
+router.get("/", userListGet);
 
 export default router;

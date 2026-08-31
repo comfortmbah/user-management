17c492874aeba8efa24b6637b649799a7c2aa3e3
sylvester <sylvestermbah90@gmail.com>
2026-08-30 17:33:42 -0700

    Add route to users create controller

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { userListGet, usersCreateGet } from "../controllers/userController.js";
+import { userListGet, usersCreateGet, usersCreatePost } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/", userListGet);
 
 router.get("/create", usersCreateGet);
+
+router.post("/create", usersCreatePost);
 
 export default router;
